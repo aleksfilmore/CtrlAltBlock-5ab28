b5fc9f397146a579389a997854aef6ae4b2ce2e8
aleksfilmore <iamalexfilip@gmail.com>
2025-07-27 09:37:43 +0200

    Update actions.js

--- app/blobs/actions.js
+++ app/blobs/actions.js
@@ -1,28 +1,25 @@
 'use server';
 
-import { set, get, list } from '@netlify/blobs';
+import { getStore } from '@netlify/blobs';
 import { uploadDisabled } from 'utils';
 
-const BLOB_BUCKET = 'shapes';
+const store = getStore({ name: 'shapes', consistency: 'strong' });
 
 export async function uploadShapeAction({ parameters }) {
   if (uploadDisabled) throw new Error('Sorry, uploads are disabled');
 
   const key = parameters.name;
-  await set(BLOB_BUCKET, key, JSON.stringify(parameters), {
-    metadata: { type: 'shape' },
-  });
+  await store.setJSON(key, parameters);
 
   console.log('Stored shape with parameters:', parameters, 'to key:', key);
 }
 
 export async function listShapesAction() {
-  const result = await list(BLOB_BUCKET);
-  const keys = result.blobs.map(({ key }) => key);
-  return keys;
+  const data = await store.list();
+  return data.blobs.map(({ key }) => key);
 }
 
 export async function getShapeAction({ keyName }) {
-  const blob = await get(BLOB_BUCKET, keyName, { type: 'json' });
-  return blob;
+  const blob = await store.get(keyName, { type: 'json' });
+  return blob ?? null;
 }
