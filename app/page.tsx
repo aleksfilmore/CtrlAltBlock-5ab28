f9236307bb5320c2c97c5e1ec3bd6789c779124b
aleksfilmore <iamalexfilip@gmail.com>
2025-07-27 08:39:15 +0200

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Welcome to CTRL+ALT+BLOCK</h1>
+      <p>The softest place to land after a hard breakup.</p>
+    </main>
+  );
+}
