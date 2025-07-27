6a1c4b527c42b0643e9c169c002b16799e8c7841
aleksfilmore <iamalexfilip@gmail.com>
2025-07-27 18:14:36 +0200

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,9 +1,11 @@
 import { NextResponse } from 'next/server';
-const protected=['/dashboard','/blobs','/closure','/report'];
-export function middleware(req){
-  if(protected.some(p=>req.nextUrl.pathname.startsWith(p))){
-    if(!req.headers.get('x-netlify-identity')){
-      return NextResponse.redirect(new URL('/signup',req.url));
+
+const protectedRoutes = ['/dashboard','/blobs','/closure','/report'];
+
+export function middleware(req) {
+  if (protectedRoutes.some(route => req.nextUrl.pathname.startsWith(route))) {
+    if (!req.headers.get('x-netlify-identity')) {
+      return NextResponse.redirect(new URL('/signup', req.url));
     }
   }
   return NextResponse.next();
