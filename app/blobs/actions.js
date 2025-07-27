020537866e70f4aff96ab6204c1f345fe67a8007
aleksfilmore <iamalexfilip@gmail.com>
2025-07-27 09:35:44 +0200

    Update actions.js

--- app/blobs/actions.js
+++ app/blobs/actions.js
@@ -1,6 +1,6 @@
 'use server';
 
-import { createBlob, getBlob, listBlobs } from '@netlify/blobs';
+import { set, get, list } from '@netlify/blobs';
 import { uploadDisabled } from 'utils';
 
 const BLOB_BUCKET = 'shapes';
@@ -9,27 +9,20 @@ export async function uploadShapeAction({ parameters }) {
   if (uploadDisabled) throw new Error('Sorry, uploads are disabled');
 
   const key = parameters.name;
-  await createBlob({
-    bucket: BLOB_BUCKET,
-    key,
-    value: JSON.stringify(parameters),
-    encoding: 'utf-8',
-    contentType: 'application/json',
+  await set(BLOB_BUCKET, key, JSON.stringify(parameters), {
+    metadata: { type: 'shape' },
   });
 
   console.log('Stored shape with parameters:', parameters, 'to key:', key);
 }
 
 export async function listShapesAction() {
-  const result = await listBlobs({ bucket: BLOB_BUCKET });
+  const result = await list(BLOB_BUCKET);
   const keys = result.blobs.map(({ key }) => key);
   return keys;
 }
 
 export async function getShapeAction({ keyName }) {
-  const blob = await getBlob({ bucket: BLOB_BUCKET, key: keyName });
-  if (!blob || !blob.body) return null;
-
-  const json = await blob.body.text();
-  return JSON.parse(json);
+  const blob = await get(BLOB_BUCKET, keyName, { type: 'json' });
+  return blob;
 }
