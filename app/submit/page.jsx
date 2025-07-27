d84d5dae80c4087d634bb04ad32e05d5e36a044f
aleksfilmore <iamalexfilip@gmail.com>
2025-07-27 10:14:32 +0200

    Add files via upload

---
+++ app/submit/page.jsx
@@ -0,0 +1,1 @@
+// Page for user submission form
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,1 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        primary: '#2bdcd2',
-        'primary-content': '#171717',
-        secondary: '#016968'
-      }
-    },
-  },
-  plugins: [],
-};
+module.exports = { content: ['./app/**/*.{js,ts,jsx,tsx}'], theme: { extend: {} }, plugins: [] };
